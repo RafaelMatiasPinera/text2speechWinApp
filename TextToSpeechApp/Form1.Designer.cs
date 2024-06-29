f33899dec1bb425beb0c61d9576839e504f2cf06
Rafael <rafael.matias.pinera@gmail.com>
2024-06-29 03:58:15 -0300

    Initial commit

---
+++ TextToSpeechApp/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TextToSpeechApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable requerida por el diseñador.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpia los recursos que se están utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; de lo contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Método requerido para el soporte del Diseñador - no modifique
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label1.Location = new System.Drawing.Point(337, 44);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(167, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text to Speech app";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(65, 257);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(711, 215);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(438, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Image = global::TextToSpeechApp.Properties.Resources.startpiola__1_;
+            this.button1.Location = new System.Drawing.Point(221, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 67);
+            this.button1.TabIndex = 4;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TextToSpeechApp.Properties.Resources.wallpaper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(868, 513);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // Declaración de los controles
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ TextToSpeechApp/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+
+namespace TextToSpeechApp
+{
+    // La clase Form1 hereda de Form, que es una clase base para crear formularios en Windows Forms
+    public partial class Form1 : Form
+    {
+        // Declara un objeto SpeechSynthesizer que se usará para convertir texto a voz
+        private SpeechSynthesizer synthesizer;
+
+        // Constructor del formulario
+        public Form1()
+        {
+            InitializeComponent();               // Inicializa los componentes del formulario
+            InitializeSpeechSynthesizer();       // Inicializa el sintetizador de voz
+        }
+
+        // Método para inicializar el sintetizador de voz
+        private void InitializeSpeechSynthesizer()
+        {
+            synthesizer = new SpeechSynthesizer();               // Crea una nueva instancia de SpeechSynthesizer
+            synthesizer.SetOutputToDefaultAudioDevice();         // Configura la salida del sintetizador al dispositivo de audio predeterminado
+
+            // Agrega las voces instaladas al comboBox1
+            foreach (var voice in synthesizer.GetInstalledVoices())
+            {
+                comboBox1.Items.Add(voice.VoiceInfo.Name);       // Añade el nombre de la voz al comboBox
+            }
+
+            // Si hay voces disponibles, selecciona la primera por defecto
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+        }
+
+        // Método que se ejecuta cuando se hace clic en el botón
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verifica si el cuadro de texto richTextBox1 está vacío
+            if (string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                MessageBox.Show("Please enter some text to speak.");  // Muestra un mensaje si el cuadro de texto está vacío
+                return;
+            }
+
+            // Si hay una voz seleccionada en el comboBox, la establece en el sintetizador
+            if (comboBox1.SelectedItem != null)
+            {
+                synthesizer.SelectVoice(comboBox1.SelectedItem.ToString());
+            }
+
+            // Convierte el texto del richTextBox1 a voz de forma asíncrona
+            synthesizer.SpeakAsync(richTextBox1.Text);
+        }
+    }
+}
+
